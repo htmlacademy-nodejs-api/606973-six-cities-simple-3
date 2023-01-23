a1c1462c9c65cf774cbb88f2af9c8b3085628564
Родин Антон Алексеевич <aalrodin@sberbank.ru>
2023-01-23 23:07:08 +0300

    add new line

--- src/common/config/config.interface.ts
+++ src/common/config/config.interface.ts
@@ -1,4 +1,4 @@
-import { ConfigSchema } from "./config.schema";
+import { ConfigSchema } from './config.schema';
 
 export interface ConfigInterface {
   get<T extends keyof ConfigSchema>(key: T): ConfigSchema[T];
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import 'reflect-metadata';
 import {Container} from 'inversify';
 import {LoggerInterface} from './common/logger/logger.interface.js';
 import LoggerService from './common/logger/logger.service.js';
@@ -8,9 +7,9 @@ import ConfigService from './common/config/config.service.js';
 import Application from './app/application.js';
 
 const applicationContainer = new Container();
-applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
-applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
-applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+applicationContainer.bind<Application>(Component.Application).to(Application);
+applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService);
+applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService);
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
