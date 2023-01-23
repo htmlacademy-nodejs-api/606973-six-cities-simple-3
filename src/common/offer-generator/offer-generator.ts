14f1c72bbff885ddf39e7c3ead8a567189b4aea7
Родин Антон Алексеевич <aalrodin@sberbank.ru>
2023-01-23 21:11:17 +0300

    add enums

--- src/common/offer-generator/offer-generator.ts
+++ src/common/offer-generator/offer-generator.ts
@@ -1,25 +1,8 @@
 import dayjs from 'dayjs';
+import { AdultsRange, BedroomsRange, CoordinatesRange, PriceRange, RatingRange, WeekDay } from '../../const.js';
 import { MockData } from '../../types/mock-data.type.js';
 import { generateRandomValue, getRandomItem, getRandomItems } from '../../utils/random.js';
 import { OfferGeneratorInterface } from './offer-generator.interface.js';
-
-const MIN_PRICE = 1000;
-const MAX_PRICE = 30000;
-
-const MIN_RATING = 0;
-const MAX_RATING = 5;
-
-const MIN_BEDROOMS = 1;
-const MAX_BEDROOMS = 7;
-
-const MIN_ADULTS = 1;
-const MAX_ADULTS = 7;
-
-const MIN_COORDINATE = 1;
-const MAX_COORDINATE = 50;
-
-const FIRST_WEEK_DAY = 1;
-const LAST_WEEK_DAY = 7;
 
 export default class OfferGenerator implements OfferGeneratorInterface {
   constructor(private readonly mockData: MockData) {}
@@ -27,23 +10,23 @@ export default class OfferGenerator implements OfferGeneratorInterface {
   public generate(): string {
     const title = getRandomItem<string>(this.mockData.titles);
     const description = getRandomItem<string>(this.mockData.descriptions);
-    const postDate = dayjs().subtract(generateRandomValue(FIRST_WEEK_DAY, LAST_WEEK_DAY), 'day').toISOString();
+    const postDate = dayjs().subtract(generateRandomValue(WeekDay.First, WeekDay.Last), 'day').toISOString();
     const city = getRandomItem<string>(this.mockData.cities);
     const previewImage = getRandomItem<string>(this.mockData.previewImages);
     const images = getRandomItems<string>(this.mockData.images).join(';');
     const isPremium = getRandomItem(['true', 'false']);
-    const rating = generateRandomValue(MIN_RATING, MAX_RATING, 1).toString();
+    const rating = generateRandomValue(RatingRange.MIN, RatingRange.MAX, 1).toString();
     const type = getRandomItem<string>(this.mockData.types);
-    const bedrooms = generateRandomValue(MIN_BEDROOMS, MAX_BEDROOMS).toString();
-    const maxAdults = generateRandomValue(MIN_ADULTS, MAX_ADULTS).toString();
-    const price = generateRandomValue(MIN_PRICE, MAX_PRICE).toString();
+    const bedrooms = generateRandomValue(BedroomsRange.MIN, BedroomsRange.MAX).toString();
+    const maxAdults = generateRandomValue(AdultsRange.MIN, AdultsRange.MAX).toString();
+    const price = generateRandomValue(PriceRange.MIN, PriceRange.MAX).toString();
     const goods = getRandomItems<string>(this.mockData.goods).join(';');
     const avatar = getRandomItem<string>(this.mockData.avatarUrls);
     const isPro = getRandomItem(['true', 'false']);
     const name = getRandomItem<string>(this.mockData.names);
     const mail = getRandomItem<string>(this.mockData.mails);
-    const latitude = generateRandomValue(MIN_COORDINATE, MAX_COORDINATE, 5).toString();
-    const longitude = generateRandomValue(MIN_COORDINATE, MAX_COORDINATE, 5).toString();
+    const latitude = generateRandomValue(CoordinatesRange.MIN, CoordinatesRange.MAX, 5).toString();
+    const longitude = generateRandomValue(CoordinatesRange.MIN, CoordinatesRange.MAX, 5).toString();
 
     const [firstname, lastname] = name.split(' ');
 
---
+++ src/const.ts
@@ -0,0 +1,29 @@
+export enum PriceRange {
+  MIN = 1000,
+  MAX = 30000
+}
+
+export enum RatingRange {
+  MIN = 0,
+  MAX = 5
+}
+
+export enum BedroomsRange {
+  MIN = 1,
+  MAX = 7
+}
+
+export enum AdultsRange {
+  MIN = 1,
+  MAX = 7
+}
+
+export enum CoordinatesRange {
+  MIN = 1,
+  MAX = 50
+}
+
+export enum WeekDay {
+  First = 1,
+  Last = 7
+}
