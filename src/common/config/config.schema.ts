9e14eb4b764521fbc15266c98585a9c3ec38a601
Родин Антон Алексеевич <aalrodin@sberbank.ru>
2023-01-23 22:31:39 +0300

    add convict to validation

--- src/common/config/config.schema.ts
+++ src/common/config/config.schema.ts
@@ -1,7 +1,12 @@
 import convict from 'convict';
+import validator from 'convict-format-with-validator';
+
+convict.addFormats(validator);
 
 export type ConfigSchema = {
   PORT: number;
+  SALT: string;
+  DB_HOST: string;
 }
 
 export const configSchema = convict<ConfigSchema>({
@@ -9,6 +14,18 @@ export const configSchema = convict<ConfigSchema>({
     doc: 'Port for incoming connections',
     format: 'port',
     env: 'PORT',
-    default: 4000
+    default: null
   },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: String,
+    env: 'SALT',
+    default: null
+  },
+  DB_HOST: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: null
+  }
 });
